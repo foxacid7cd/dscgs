ce776850ae9e206fd3a356a151903c25564d27c5
Yevhenii Matviienko <foxacid@keemail.me>
2024-01-01 22:28:21 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import monkey from "vite-plugin-monkey";
+import { name, version, author } from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    monkey({
+      entry: "src/main.ts",
+      userscript: {
+        name,
+        version,
+        author: author.name,
+        match: ["https://www.discogs.com/*"],
+      },
+      server: {
+        open: false,
+      },
+    }),
+  ],
+  server: {
+    host: "127.0.0.1",
+    origin: "https://yevheniis-macbook-pro.tailnet-9fda.ts.net",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5174,
+    },
+  },
+});
